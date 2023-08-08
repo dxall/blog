b5079cb1549fd48e6ff440fc37c23ef2283f2b17
ubuntu <ubuntu@localhost.localdomain>
2023-08-08 16:01:04 +0800

    first commit

---
+++ .vitepress/config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "dx blog",
+  description: "fe question blog",
+  lastUpdated: true,
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'ChatGPT', link: 'https://next.dxall.top' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/dxall' }
+    ],
+  }
+})
